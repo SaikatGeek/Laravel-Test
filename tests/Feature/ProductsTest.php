df542b8c126c67adf15053692b5fe89cbbb86df9
SaikatGeek <saikat.geek@gmail.com>
2022-04-16 12:35:14 +0600

    Test: Product availability change

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -6,10 +6,10 @@ use App\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class ProductsTest extends TestCase
 {
-
     use RefreshDatabase;
+    
 
     public function test_homepage_contains_empty_products_table()
     {
@@ -20,6 +20,18 @@ class ExampleTest extends TestCase
         $response->assertSee('No products found');
     }
 
-    
+    public function test_homepage_contains_non_empty_products_table()
+    {
+        $data = Product::create([
+            "name" => "Mobile",
+            "price" => 56
+        ]);
+
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+
+        $response->assertDontSee('No products found');
+    }
 
 }
