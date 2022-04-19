5c1755cfd801a51f63b4bb73b0155d4eb68046a5
SaikatGeek <saikat.geek@gmail.com>
2022-04-19 23:45:23 +0600

    Currency Converter Added

--- app/Product.php
+++ app/Product.php
@@ -2,9 +2,14 @@
 
 namespace App;
 
+use App\Services\CurrencyService;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     protected $fillable = ['name', 'price'];
+
+    public function getPriceEurAttribute(){
+        return (new CurrencyService)->convert($this->price, 'usd', 'eur');
+    }
 }
