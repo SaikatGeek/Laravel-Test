0cc9545d2654e32daa49287f010c1f4ee8331fba
SaikatGeek <saikat.geek@gmail.com>
2022-04-22 19:28:59 +0600

    Test: CurrencyTest Completed

--- tests/Unit/CurrencyTest.php
+++ tests/Unit/CurrencyTest.php
@@ -16,10 +16,28 @@ class CurrencyTest extends TestCase
      */
     public function test_currency_convert_usd_to_eur()
     {
-        $usd_amount = 100;
+        $amount_in_usd = 100;
 
-        $this->assertEquals(89, 
-            (new CurrencyService)->convert($usd_amount, 'usd', 'eur')
+        $this->assertEquals( 89, 
+            (new CurrencyService)->convert($amount_in_usd, 'usd', 'eur')
+        );
+    }
+
+    public function test_currency_convert_gbp_to_euro()
+    {
+        $amount_in_gbp = 100;
+
+        $this->assertEquals( 0, 
+            (new CurrencyService)->convert($amount_in_gbp, 'gbp', 'euro')
+        );
+    }
+
+    public function test_currency_convert_usd_to_gbp()
+    {
+        $amount_in_usd = 100;
+
+        $this->assertEquals( 0,
+            (new CurrencyService)->convert($amount_in_usd, 'usd', 'gbp')
         );
     }
     
